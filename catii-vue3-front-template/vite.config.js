fb854dae8c3ed385ea0a805e87cdd6169cbd7bca
guoqinhuang <guoqinhuang@MacBook-Pro.local>
2022-06-28 16:37:24 +0800

    fix:dev server config error

--- catii-vue3-front-template/vite.config.js
+++ catii-vue3-front-template/vite.config.js
@@ -18,13 +18,19 @@ export default () =>
     build: {
       chunkSizeWarningLimit: 1600,
     },
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:3000/api', // 代理的目标地址
-        changeOrigin: true, // 开发模式，默认的origin是真实的 origin:localhost:3000 代理服务会把origin修改为目标地址
-        // secure: true, // 是否https接口
-        // ws: true, // 是否代理websockets
-        // rewrite: (path) => path.replace(/^\/api/, '') // 路径重写
+    server: {
+      port: 3000,
+      strictPort: true,
+      watch: {
+        usePolling: true,
+      },
+      hmr: true,
+      proxy: {
+        '/cgi/': {
+          target: 'http://127.0.0.1:8000/', // 代理的目标地址
+          rewrite: (path) => path.replace(/cgi/, ''), // 路径重写
+          changeOrigin: true,
+        },
       },
     },
   });
